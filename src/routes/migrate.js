ee0bfc743102de276c79f0d496aad17ca1299b11
Damian Soto <soto.platero@gmail.com>
2021-08-17 19:35:55 -0400

    update

--- src/routes/migrate.js
+++ src/routes/migrate.js
@@ -5,7 +5,7 @@ import { nanoid } from 'nanoid'
 export async function post() {
 
 	let blogs = await db.all()
-	blogs = blogs.map( el => ({...el, id: nanoid()}) )
+	blogs = blogs.map( el => ({...el, id: 'id'+nanoid()}) )
 	// // console.log(blogs)
     await db.save(blogs)
 	// notify(`Nuevo Blog: ${data.title} ${data.url}`)
