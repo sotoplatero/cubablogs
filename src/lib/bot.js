a33a99ed2420b3f628ec93d50d4ddbca918006c0
Damian Soto <soto.platero@gmail.com>
2021-08-29 14:27:04 -0400

    update notify bot

--- src/lib/bot.js
+++ src/lib/bot.js
@@ -1,14 +1,16 @@
 // import {Telegram} from 'telegraf'
 import TelegramBot from 'node-telegram-bot-api';
 
+const CHANNEL_PUBLIC_ID = -1001518905562 
+const CHANNEL_ADMIN_ID = -1001544550528 
+const BOT_TOKEN = import.meta.env.VITE_BOT_TOKEN
 
-const CHANNEL_PUBLIC_ID = import.meta.env.VITE_LOCAL ? -1001544550528 : -1001518905562 
+const bot = new TelegramBot( BOT_TOKEN, { polling: false } );
 
-function notify (msg, channel = 'public' ) {
+function notify(msg, channel = 'public') {
 
-	CHANNEL_ID = channel === 'admin' ? -1001544550528 : CHANNEL_PUBLIC_ID
+	CHANNEL_ID = (channel === 'public')  ? CHANNEL_PUBLIC_ID : CHANNEL_ADMIN_ID
 
-	const bot = new TelegramBot(import.meta.env.VITE_BOT_TOKEN, {polling: false});
 	try {
 		bot.sendMessage( CHANNEL_ID, msg, { parse_mode: 'Markdown'} )
 	} catch (err) {
