40969e450dbf0cb2b2069bd3f234df2567202a49
Damian Soto <soto.platero@gmail.com>
2021-09-09 21:48:10 -0400

    update

--- src/lib/components/avatar.svelte
+++ src/lib/components/avatar.svelte
@@ -3,7 +3,7 @@
 	export { _class as class}
 	let _class = "w-6 h-6"
 
-	const colors = ["text-red-500","text-red-700", "text-green-500",  "text-blue-500", "text-indigo-500", "text-indigo-700","text-purple-500","text-purple-700","text-yellow-600","text-yellow-700","text-pink-500","text-pink-700"]
+	const colors = ["text-red-500","text-red-700", "text-green-500", "text-blue-500", "text-indigo-500", "text-indigo-700","text-purple-500","text-purple-700","text-yellow-600","text-yellow-700","text-pink-500","text-pink-700"]
 
 	let color = colors[ Math.floor( Math.random() * colors.length ) ] 
 
@@ -12,7 +12,7 @@
 	{#if (blog.logo || blog.twitter)}
 		<img 
 			src="{ blog.logo || blog.twitter.avatar }" 
-			alt="{blog.title}" 
+			alt="{ blog.title }" 
 			class="{_class} object-center object-cover  ">
 	{:else}
 
