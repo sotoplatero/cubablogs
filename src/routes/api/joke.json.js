45a56bf489251eb1e4d770c86042f71287f7af23
Damian Soto <soto.platero@gmail.com>
2021-10-08 05:55:01 -0400

    update

--- src/routes/api/joke.json.js
+++ src/routes/api/joke.json.js
@@ -1,42 +1,28 @@
 import cheerio from 'cheerio'
 import '$lib/random'
-import './_urls_jokes.js'
+import urls from './_urls_jokes.js'
 
 export async function get() {
   try {
 
-    let urls = [
-      'https://chistes.yavendras.com',
-      // 'http://www.todo-chistes.com/chistes-al-azar',
-      'https://www.1000chistes.com/',
-    ] 
     let data = [], response, body, $, menu;
-    let url = urls.random() 
+    const url = urls.random()
     response = await fetch( url );
     body = await response.text();
     $ = cheerio.load( body );
-
+    console.log(url)
     if ( /yavendras/g.test(url) ) {
-      // abrir un menu aleatorio
-      let randomMenu = $('#HUMOR .submenu a[href$=".php"]').map((i,a)=>$(a).attr('href')).get().random();
-      response = await fetch( 'https:'+randomMenu );
+      // seleccionar una de las paginas
+      let pageTo = $( $('.pagination a').toArray().random() ).attr('href')
+      response = await fetch( `https:${pageTo}` );
       body = await response.text();
       $ = cheerio.load( body );
 
-      // seleccionar una de las paginas
-      let pages = ( $('.pagination').first().find('li').length - 4)
-      if ( pages > 1 )  {
-        let randomPage = Math.floor( (Math.random() * pages) + 1 )
-        response = await fetch( `https:${randomMenu}?pagina_actual=${randomPage}` );
-        body = await response.text();
-        $ = cheerio.load( body );
-      }
-
-      let jokes = $('.search-result-item-body .row.fila')
-      let indexJoke = Math.floor(Math.random() * jokes.length)
-      $joke = $(jokes[ indexJoke ])
+      let joke = $('.search-result-item-body .row.fila').toArray().random()
+      $joke = $(joke)
 
       data = {
+        title: $joke.find('h4').text().trim(),
         url: 'http:' + $joke.find('.search-result-item-heading a').attr('href'),
         content: $joke.find('.description').html().trim(),
       }
@@ -44,32 +30,33 @@ export async function get() {
 
     if ( /1000chistes/g.test(url) ) {
 
-      const categoryUrl = $( $('[itemprop="itemListElement"] > a').toArray().random() ).attr('href')
-      const response1000chistes = await fetch(categoryUrl)
+      const pageUrl = $( $('#nav-links > a').toArray().random() ).attr('href')
+      const response1000chistes = await fetch(`https://www.1000chistes.com${pageUrl}`)
       const body1000chistes = await response1000chistes.text()
       $ = cheerio.load( body1000chistes );
 
       $joke = $( $('.chiste').toArray().random() )
       data = {
+        title: $joke.find('h2').text().trim(),
         url: $joke.find('h2 > a').attr('href'),
         content: $joke.find('[itemprop="articleBody"]').html(),
       }
-      // 'itemprop="articleBody"'
+
     }
 
     if ( /todo-chistes/g.test(url) ) {
+      console.log($('article.node h2'))
       data = {
-        url: ' http://www.todo-chistes.com/' + $('.view-content article header h2 a').attr('href'),
-        content: $('.field-chistes')
-          .map( (i,el) => $(el)?.html() )
-          .get().trim(),
+        title: $('article.node h2').text().trim(),
+        url: 'http://www.todo-chistes.com' + $('article.node h2 a').attr('href'),
+        content: $('article.node .field-chistes,article.node .field-chiste').html().trim()
       }
     }
 
     if ( /loschistes/g.test(url) ) {
       data = {
-        url: ' http://www.todo-chistes.com/' + $('article header h2 a').attr('href'),
-        content: $('.field-chistes').html().trim(),
+        url: `http://www.todo-chistes.com/${ $('article header h2 a').attr('href') }`,
+        content: $('article .field-chistes').html().trim(),
       }
     }
 
