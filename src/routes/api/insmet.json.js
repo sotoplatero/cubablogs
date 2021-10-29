373fd7401d58d4794eb700704f034aa4c0464a28
Damian Soto <soto.platero@gmail.com>
2021-10-29 16:09:36 -0400

    add rss insmet

--- src/routes/api/insmet.json.js
+++ src/routes/api/insmet.json.js
@@ -5,25 +5,26 @@ export async function get() {
   const selectorMap = '.bordeBlanco > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > p:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > p:nth-child(2)'
 
   try {
-    const decoder = new TextDecoder("iso-8859-1");    
-    const res = await fetch( 'http://www.insmet.cu/asp/genesis.asp?TB0=PLANTILLAS&TB1=PTM&TB2=/Pronostico/Ptm.txt' )
-    let html = await res.arrayBuffer()
+    const decoder = new TextDecoder("iso-8859-1");   
+    const url  = 'http://www.insmet.cu/asp/genesis.asp?TB0=PLANTILLAS&TB1=PTM&TB2=/Pronostico/Ptm.txt'
+    const res = await fetch( url )
+    let html = await res.text()
     // console.log(html)
-    const $ = cheerio.load( decoder.decode(html) );
+    const $ = cheerio.load( html );
     const $table = $('.contenidoPagina[valign="top"]')
+
     const data = {
-      // html: $table.html().trim(),
+      url,
       title: $table.find('b').first().html(),
       content: $table.find('p[align="justify"]').html(),
       author: $table.find('[id^="name"] ').map(el=>$(el).html()).get(),
       // avatar: `http://www.insmet.cu` + $table.find('#autor1').attr('src').replace(/^\.\./,''),
-      map: $(selectorMap).html().trim(),
+      map: $(selectorMap).html().trim().replace(/\.{2}/g,'http://www.insmet.cu'),
       // tmax: $('#tmax').text().replace(/\D/g,''),
       // tmin: $('#tmin').text().replace(/\D/g,''),
       // state: $('#estado').text(),
-      icon: `http://www.insmet.cu` + $('table[summary] #icono').attr('src').replace(/^\.\./,''),
+      icon: `http://www.insmet.cu` + $('table[summary] #icono').attr('src'),
     }
-    console.log(data)
     return {
       body: {...data},
     }
