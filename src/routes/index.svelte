9e01f6617936e95ba690220f21143d74daf1a236
Damian Soto <soto.platero@gmail.com>
2021-11-03 08:56:53 -0400

    add popular section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,32 +40,6 @@
 </div> -->
 <div class="mb-12 space-y-24">
 
-
-
-<div id="revue-embed">
-  <form action="http://newsletter.cubablog.net/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
-  <div class="revue-form-group">
-    <label for="member_email">Email address</label>
-    <input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email">
-  </div>
-  <div class="revue-form-group">
-    <label for="member_first_name">First name <span class="optional">(Optional)</span></label>
-    <input class="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
-  </div>
-  <div class="revue-form-group">
-    <label for="member_last_name">Last name <span class="optional">(Optional)</span></label>
-    <input class="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
-  </div>
-  <div class="revue-form-actions">
-    <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
-  </div>
-  <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
-  </form>
-</div>
-
-
-
-
 	<div>
 		<div class="md:flex md:items-center space-y-6 md:space-y-0 md:space-x-8">
 
