f15e914ca6f0bef1b81455a6b1fd22784750156f
Damian Soto <soto.platero@gmail.com>
2021-11-04 06:32:30 -0400

    update

---
+++ src/lib/util/getPopular.js
@@ -0,0 +1,39 @@
+import { getHostname } from 'tldts'
+import { unfurl } from 'unfurl.js'
+
+const TOKEN = import.meta.env.VITE_UMAMI_TOKEN
+
+export default async () => {
+
+    const today = new Date
+    const endAt = today.valueOf()
+    const startAt = today.setDate( today.getDate() - 7 );
+    
+    const url = `https://umami.dsoto.dev/api/website/9/metrics?type=url&start_at=${startAt}&end_at=${endAt}`
+    const res = await fetch(url, {
+        headers: { 'Cookie': `umami.auth=${TOKEN}` }
+    })
+    
+    const urls = (await res.json())
+        .filter( ({ x:url }) => url.split('/')[2] != '146' && /^\/post/.test(url) )
+        .filter( ( ele, index ) => index < 10 )
+
+    return await Promise.all(urls.map( async ({ x, y: counter }) => {
+
+        const url = `https://${ x.replace(/\/post\/\d+\//,'') }`
+        const ogData = await unfurl( url )
+    
+        return {
+            url: url,
+            title: ogData.title,
+            description: ogData.description,
+            image: ogData.twitter_card?.images?.url || ogData.open_graph?.images?.url,
+            blog: x.split('/')[2],
+            hostname: getHostname(url),
+            twitter: ogData.twitter_card?.site || ogData.twitter_card?.creator,
+            counter
+        }
+    
+    }))
+
+}
---
+++ src/routes/post/related/[id].json.js
@@ -0,0 +1,55 @@
+import supabase from '$lib/supabase'
+import getPost from '$lib/post'
+
+/**
+ * @type {import('@sveltejs/kit').Load}
+ */
+export async function get({query}) {
+	const url = encodeURI(query.get('url'))
+	const idBlog = parseInt(url)
+
+	let { data: blog, error } = await supabase
+		.from('blogs')
+		.select('*')
+		.eq('id', idBlog)
+		.single()	
+
+	if (error) {
+		return {
+			status: 404,
+			body: {}
+		}		
+	}
+
+	const link = url.replace(/^\d+\//,'').toLowerCase()
+	const post = ( blog.post.url.toLowerCase().indexOf(link) < 0 ) 
+		? await getPost( blog.rss, link )
+		: blog.post
+
+	return {
+		headers: { 
+			'Cache-Control': `s-maxage=1, stale-while-revalidate`,
+		},		
+		body: { 
+			...post,
+			link: `/post/${blog.id}/${post.url.replace(/https?:\/\//,'')}`,
+			blog: {
+				id: blog.id,
+				title: blog.title,
+				url: blog.url,
+				logo: blog.logo,
+				publisher: blog.hostname.replace(/^www\./,''),
+			},
+			// related: blog.items
+			// 	.filter( el => el.link.toLowerCase().indexOf(link) === -1 )
+			// 	.shuffle()
+			// 	.slice(0,2)
+			// 	.map( el => ({
+			// 		title: el.title,
+			// 		link: el.link,
+			// 		url: `/post/${blog.id}/${el.link.replace(/https?:\/\//,'')}`
+			// 	}))
+		}
+	};
+
+}
