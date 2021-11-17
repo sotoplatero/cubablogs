9a940bf38166645ffd19d8e3e6d5a0740b3d487e
Damian Soto <soto.platero@gmail.com>
2021-11-17 10:35:59 -0500

    image src

--- src/routes/img/[width]/[height]/[...url].js
+++ src/routes/img/[width]/[height]/[...url].js
@@ -1,40 +1,40 @@
-import sharp from 'sharp'
+// import sharp from 'sharp'
 
-export async function get({params, query}) {
+// export async function get({params, query}) {
 
-    const res = await fetch(`https://${params.url}`)
-	if (res.ok) {
-        // const resImage = await fetch(src)
-        const imageResponse = await res.arrayBuffer()
-        const imageBuffer = Buffer.from(imageResponse, 'binary')
+//     const res = await fetch(`https://${params.url}`)
+// 	if (res.ok) {
+//         // const resImage = await fetch(src)
+//         const imageResponse = await res.arrayBuffer()
+//         const imageBuffer = Buffer.from(imageResponse, 'binary')
         
-        const imageBufferResize = await sharp(imageBuffer)
-          .resize({
-            width: parseInt(params.width),
-            height: parseInt(params.height),
-            fit: 'inside',
-            // position: sharp.strategy.entropy
-          })
-          .toBuffer()  
+//         const imageBufferResize = await sharp(imageBuffer)
+//           .resize({
+//             width: parseInt(params.width),
+//             height: parseInt(params.height),
+//             fit: 'inside',
+//             // position: sharp.strategy.entropy
+//           })
+//           .toBuffer()  
 
-        const img = Buffer.from(imageBufferResize, 'binary')
+//         const img = Buffer.from(imageBufferResize, 'binary')
 
-        return {
-            headers: { 
-                // 'Content-Disposition': `attachment; filename="${params.url.replace('/','-')}"`,
-                'content-type': res.headers.get('content-type'),
-                'Cache-Control': 's-maxage=1, stale-while-revalidate',
-            },
-            body: img
-        }; 
-    }
+//         return {
+//             headers: { 
+//                 // 'Content-Disposition': `attachment; filename="${params.url.replace('/','-')}"`,
+//                 'content-type': res.headers.get('content-type'),
+//                 'Cache-Control': 's-maxage=1, stale-while-revalidate',
+//             },
+//             body: img
+//         }; 
+//     }
     
-    return {
-        headers: { 
-            'Cache-Control': `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`,
-        }, 
-        body: ''
-    }
+//     return {
+//         headers: { 
+//             'Cache-Control': `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`,
+//         }, 
+//         body: ''
+//     }
 
 
-}
+// }
